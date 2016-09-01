3efc2c1fee5f437c31b6978ab0ff45e4e0040c8e
TsytsorinDA <dtsytsorin@gmail.com>
2016-09-01 20:40:43 +0400

    Create Constructor

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -7,9 +7,20 @@ namespace Snake
 {
     class Point
     {
-        public int x;
-        public int y;
-        public char sym;
+        private int x;
+        private int y;
+        private char sym;
+
+        public Point()
+        {
+
+        }
+        public Point(int m_x, int m_y, char m_sym)
+        {
+            x = m_x;
+            y = m_y;
+            sym = m_sym;
+        }
 
         public void Draw()
         {
