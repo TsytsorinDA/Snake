427ca3d81b89c4556628e7916d221435a37e0a76
TsytsorinDA <dtsytsorin@gmail.com>
2016-09-05 22:02:51 +0400

    class wall

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -14,17 +14,8 @@ namespace Snake
 
             Console.SetBufferSize(80, 25);
 
-            Console.ForegroundColor = ConsoleColor.Green;
-
-            HorizontalLine Hline = new HorizontalLine(0, 78, 0, '+');
-            Hline.Drow();
-            Hline = new HorizontalLine(0, 78, 24, '+');
-            Hline.Drow();
-            VerticalLine Vline = new VerticalLine(0, 24, 0, '+');
-            Vline.Drow();
-            Vline = new VerticalLine(0, 24, 78, '+');
-            Vline.Drow();
-
+            Walls wall = new Walls(80, 25);
+            wall.Draw();
 
             Snake snake = new Snake(new Point(78 / 2, 24 / 2, '*'), 4, Direction.Down);
             snake.Drow();
@@ -35,6 +26,11 @@ namespace Snake
 
             while (true)
             {
+                if(wall.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
+
                 if (snake.Eat(food))
                 {
                     food = foodcreator.CreateFood(snake.GetSnake());
